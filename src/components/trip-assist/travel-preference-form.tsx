43af260e1fc7ffe2877573417cd47e3ca22225f4
Balachandran Janaki Raman <Balachandran.jr@gmail.com>
2025-07-16 02:13:37 +0000

    can you make this more attractive form?

--- src/components/trip-assist/travel-preference-form.tsx
+++ src/components/trip-assist/travel-preference-form.tsx
@@ -30,7 +30,6 @@ import {
 } from "@/components/ui/select";
 import { Textarea } from "@/components/ui/textarea";
 import { Checkbox } from "@/components/ui/checkbox";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 
