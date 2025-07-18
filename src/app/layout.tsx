5ed573a60074a66105ff3c96ca288a7337b2ffb9
Balachandran Janaki Raman <Balachandran.jr@gmail.com>
2025-07-18 04:58:26 +0000

    better remove it..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
-import Logo from '@/components/logo';
 
 export const metadata: Metadata = {
   title: 'TripAssist',
@@ -21,9 +20,6 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased relative">
-        <header className="absolute top-0 right-0 p-4 z-10">
-          <Logo />
-        </header>
         {children}
         <Toaster />
       </body>
